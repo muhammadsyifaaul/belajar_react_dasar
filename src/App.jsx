147309707d27ac2c5d662c00ae40c0ce8f8566e3
muhammadsyifaaul <muhammadsyifaaulj@gmail.com>
2024-09-21 20:43:22 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -13,8 +13,63 @@ function  Nav() {
     </>
   )
 }
+function Name(props) {
+
+  return (
+    <h1>Hello {props.nama ? props.nama : "Fulan"}</h1>
+  )
+}
+function Pekerjaan({nama,job}) {
+
+  return (
+    <>
+    <Name nama = "jamal"/>
+    <h1>Hi {nama} you are {job}</h1>
+    </>
+  )
+}
+function Mahasiswa() {
+  const Students = ['jamal','abdul','nasser'];
+  return (
+    <>
+    <ul>
+      {
+      Students.map((student) => {
+        console.log(student)
+        return <li key={student}>{student}</li>;
+      })
+    }
+    </ul>
+    </>
+  )
+}
+function Button({tipe,onClick}) {
+
+  return (
+
+    <button onClick={onClick}>{tipe ? tipe : '+'}</button>
+
+  )
+}
+function Nilai({ count }) {
+  return (
+    <span>{count}</span> 
+  );
+}
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  function decrement() {
+    setCount((prevCount) => prevCount - 1);
+  }
+
+  function increment() {
+    setCount((prevCount) => prevCount + 1);
+  }
+
+  function reset() {
+    setCount(0);
+  }
 
   return (
     <>
@@ -39,8 +94,20 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      <Name nama="Budi" />
+      <Pekerjaan job="Programmer" nama='Samsul' />
+      <Mahasiswa />
+
+      <Button tipe="-" onClick={decrement} />
+
+      <Nilai count={count} />
+
+      <Button onClick={increment} />
+
+      <Button tipe='reset' onClick={reset}/>
     </>
-  )
+  );
 }
 
 export default App
